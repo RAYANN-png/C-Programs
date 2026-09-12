4d00e99d79f23b1a900df187d6ca1909bf76ed7c
RAYANN-png <muciraryan@gmail.com>
2026-09-12 11:15:46 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,23 @@
+/*
+Author:Ryan Munene
+Registration Number:BCS-05-0539
+Description:Units offered this sememster
+Date:10/09/2026
+Dev-C++
+*/
+
+#include <stdio.h>
+
+int main() {
+	//display the units offered this semester.
+	printf ("1.Introduction to Programming\n" );
+	printf ("2.Data Structures\n" );
+	printf ("3.Computer Networks\n");
+	printf ("4.Database Systems\n");
+	printf ("5.Software Engineering\n");
+	
+	return 0;
+	
+	
+	
+}
