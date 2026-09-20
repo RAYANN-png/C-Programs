fad811816b1fe250a9169d4c0a3fcbfd0789b356
RAYANN-png <muciraryan@gmail.com>
2026-09-21 02:17:42 +0300

    Refactor user_input_program.c with comments
    Added comments to clarify user input prompts.

--- user_input_program.c
+++ user_input_program.c
@@ -11,18 +11,20 @@ Version 1
 int main() {
 	float height; //%f
 	double bank_balance; //%lf
-	char phone[20] ; //%s
+	char phone[20] ; //%s(because 0 is not an integer)
 	
-	printf("Enter your height in cm: \t");
+	printf("Enter your height in cm: \t");//prompts user to input height
 	scanf("%f", &height);
 	
-	printf("Enter your Bank Balance in Ksh: \t");
+	printf("Enter your Bank Balance in Ksh: \t");//prompts user to input bank balance
 	scanf("%lf", &bank_balance);
 	
-	printf("Enter your Phone number: \t");
+	printf("Enter your Phone number: \t");//prompt user to inptu phone number
 	scanf("%s", &phone);
 	
 	printf("Height: %.1f \n", height);
 	printf("Bank Balance: %.2lf \n", bank_balance);
 	printf("Phone Number: is %s \n", phone);
+
+	return 0;
 }
